de23c9846c04aae10238bd9056b7ee5251637e02
huqisuangs <huqishuangs@gmail.com>
2022-12-14 20:40:46 +0800

    更换CMD

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.optim.lr_scheduler as lr_scheduler
-import torch.utils.data
+ 
 import yaml
 from torch.cuda import amp
 from torch.nn.parallel import DistributedDataParallel as DDP
